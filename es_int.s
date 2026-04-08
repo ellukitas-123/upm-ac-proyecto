12dc9f9babc001c9fe1dd97180d6aff9de967f93
Lucas Winand <lucas.winand@hotmail.es>
2026-04-08 10:51:35 +0200

    Format and comments

--- es_int.s
+++ es_int.s
@@ -1,59 +1,65 @@
 * Definici�n de equivalencias
 *********************************
 
-MR1A    EQU     $EFFC01       * de modo A (escritura)
-MR2A    EQU     $EFFC01       * de modo A (2� escritura)
-SRA     EQU     $EFFC03       * de estado A (lectura)
-CSRA    EQU     $EFFC03       * de seleccion de reloj A (escritura)
-CRA     EQU     $EFFC05       * de control A (escritura)
-TBA     EQU     $EFFC07       * buffer transmision A (escritura)
-RBA     EQU     $EFFC07       * buffer recepcion A  (lectura)
-ACR		EQU		$EFFC09	      * de control auxiliar
-IMR     EQU     $EFFC0B       * de mascara de interrupcion A (escritura)
-ISR     EQU     $EFFC0B       * de estado de interrupcion A (lectura)
-MR1B    EQU     $EFFC11       * de modo B (escritura)
-MR2B    EQU     $EFFC11       * de modo B (2� escritura)
-CRB     EQU     $EFFC15	      * de control A (escritura)
-TBB     EQU     $EFFC17       * buffer transmision B (escritura)
-RBB		EQU		$EFFC17       * buffer recepcion B (lectura)
-SRB     EQU     $EFFC13       * de estado B (lectura)
-CSRB	EQU		$EFFC13    	  * de seleccion de reloj B (escritura)
+MR1A	EQU		$EFFC01			* de modo A (escritura)
+MR2A	EQU		$EFFC01			* de modo A (2� escritura)
+SRA		EQU		$EFFC03			* de estado A (lectura)
+CSRA	EQU		$EFFC03			* de seleccion de reloj A (escritura)
+CRA		EQU		$EFFC05			* de control A (escritura)
+TBA		EQU		$EFFC07			* buffer transmision A (escritura)
+RBA		EQU		$EFFC07			* buffer recepcion A  (lectura)
+ACR		EQU		$EFFC09			* de control auxiliar
+IMR		EQU		$EFFC0B			* de mascara de interrupcion A (escritura)
+ISR		EQU		$EFFC0B			* de estado de interrupcion A (lectura)
+MR1B	EQU		$EFFC11			* de modo B (escritura)
+MR2B	EQU		$EFFC11			* de modo B (2� escritura)
+CRB		EQU		$EFFC15			* de control A (escritura)
+TBB		EQU		$EFFC17			* buffer transmision B (escritura)
+RBB		EQU		$EFFC17			* buffer recepcion B (lectura)
+SRB		EQU		$EFFC13			* de estado B (lectura)
+CSRB	EQU		$EFFC13			* de seleccion de reloj B (escritura)
 
-IVR		EQU		$EFFC19		  	* Vector de interrupcion
-ISR		EQU 	$EFFC0B			* Estado de interrupcion
-IMR		EQU		$EFFC0B		  	* Mascara de interrupcion
+IVR		EQU		$EFFC19			* Vector de interrupcion
+ISR		EQU		$EFFC0B			* Estado de interrupcion
+IMR		EQU		$EFFC0B			* Mascara de interrupcion
 
 
-CR		EQU		$0D	      		* Carriage Return
-LF		EQU		$0A	      		* Line Feed
-FLAGT	EQU		2	      		* Flag de transmisi�n
-FLAGR   EQU     0	      		* Flag de recepci�n
+CR		EQU		$0D				* Carriage Return
+LF		EQU		$0A				* Line Feed
+FLAGT	EQU		2				* Flag de transmisi�n
+FLAGR	EQU		0					* Flag de recepci�n
 
 IMRCP:	DC.B	0				* Copia del IMR en memoria (IMR no es legible)
 
+******************************
+************ INIT ************
+******************************
 INIT:
-	MOVE.B          #%00010000,CRA      * Reiniciar a MR1A
-	MOVE.B          #%00010000,CRB      * Reiniciar a MR1B
-	MOVE.B          #%00000011,MR1A     * establecer 8 bits/caracter A
-	MOVE.B          #%00000011,MR1B     * establecer 8 bits/caracter B
-	MOVE.B          #%00000000,MR2A     * Desactivar eco A
-	MOVE.B          #%00000000,MR2B     * Desactivar eco B
-	MOVE.B          #%11001100,CSRA     * 38400 bps
-	MOVE.B          #%11001100,CSRB     * 38400 bps
-	MOVE.B          #%00000000,ACR      
-	MOVE.B          #%00000101,CRA      * Full duplex
-	MOVE.B          #%00000101,CRB      * Full duplex
+	MOVE.B			#%00010000,CRA		* Reiniciar a MR1A
+	MOVE.B			#%00010000,CRB		* Reiniciar a MR1B
+	MOVE.B			#%00000011,MR1A		* establecer 8 bits/caracter A
+	MOVE.B			#%00000011,MR1B		* establecer 8 bits/caracter B
+	MOVE.B			#%00000000,MR2A		* Desactivar eco A
+	MOVE.B			#%00000000,MR2B		* Desactivar eco B
+	MOVE.B			#%11001100,CSRA		* 38400 bps
+	MOVE.B			#%11001100,CSRB		* 38400 bps
+	MOVE.B			#%00000000,ACR
+	MOVE.B			#%00000101,CRA		* Full duplex
+	MOVE.B			#%00000101,CRB		* Full duplex
 
-	MOVE.B 			#$40,IVR			* Vector de interrupción
+	MOVE.B			#$40,IVR			* Vector de interrupción
 
-	MOVE.B  		#%00100010,IMRCP	* Guardar copia del IMR
-	MOVE.B 			IMRCP,IMR			* Habilitar interrupciones
+	MOVE.B			#%00100010,IMRCP	* Guardar copia del IMR
+	MOVE.B			IMRCP,IMR			* Habilitar interrupciones
 
 	MOVE.L			#RTI,$100			* Añadir la dirección de la RTI a la tabla de vectores de interrupcion
 
-	BSR 			INI_BUFS
+	BSR				INI_BUFS
 	RTS
 
+******************************
+************ SCAN ************
+******************************
 SCAN:
 	LINK			A6,#-8				* Marco de pila
 
@@ -61,14 +67,14 @@ SCAN:
 	MOVE.W			12(A6),D1			* Descriptor
 	MOVE.W			14(A6),D2			* Tamano
 
-	CMP.W			#0,D1
+	CMP.W			#0,D1				* Comprobar descriptor valido
 	BEQ				scan_preparacion
 	CMP.W			#1,D1
 	BEQ				scan_preparacion
 
 scan_error:
-	MOVE.L  #-1,D0         * Poner código de error
-    BRA     scan_fin
+	MOVE.L			#-1,D0						* Poner código de error
+	BRA				scan_fin
 
 scan_preparacion:
 	MOVE.W			#0,D3
@@ -79,28 +85,32 @@ scan_bucle:
 
 	MOVE.L			D1,D0
 
-    MOVE.L  		A0,-4(A6)			* Guardar *buffer
-    MOVE.L  		D3,-8(A6)			* Guardar contador
-	BSR LEECAR
-	MOVE.L  		-8(A6),D3			* Recuperar contador
-    MOVE.L  		-4(A6),A0			* Recuperar *buffer
-    MOVE.L  		12(A6),D1			* Recuperar descriptor
-	MOVE.L  		14(A6),D2			* Recuperar Tamano
+	MOVE.L			A0,-4(A6)			* Guardar *buffer
+	MOVE.L			D3,-8(A6)			* Guardar contador
+	BSR				LEECAR
+	MOVE.L			-8(A6),D3			* Recuperar contador
+	MOVE.L			-4(A6),A0			* Recuperar *buffer
+	MOVE.L			12(A6),D1			* Recuperar descriptor
+	MOVE.L			14(A6),D2			* Recuperar Tamano
 
-	CMP.L			#-1,D0
+	CMP.L			#-1,D0				* Comprobar si hay error
 	BEQ				scan_bucle_fin
-	MOVE.B 			D0,(A0)+
 
-	ADD.W			#1,D3
+	MOVE.B			D0,(A0)+			* Copiar caracter al buffer
+
+	ADD.W			#1,D3				* Sumar contador
 	BRA				scan_bucle
 
 scan_bucle_fin:
-	MOVE.L			D3,D0
+	MOVE.L			D3,D0				* Valor de retorno (caracteres escritos)
 
 scan_fin:
-	UNLK			A6
+	UNLK			A6					* Recuperar marco de pila
 	RTS
 
+*******************************
+************ PRINT ************
+*******************************
 PRINT:
 	LINK			A6,#-12				* Marco de pila
 
@@ -108,18 +118,18 @@ PRINT:
 	MOVE.W			12(A6),D2			* Descriptor
 	MOVE.W			14(A6),D3			* Tamano
 
-	CMP.W			#0,D2
+	CMP.W			#0,D2				* Comprobar descriptor valido
 	BEQ				print_preparacion
 	CMP.W			#1,D2
 	BEQ				print_preparacion
 
 print_error:
-	MOVE.L  		#-1,D0         * Poner código de error
-    BRA     		print_fin
+	MOVE.L			#-1,D0				* Poner código de error
+	BRA				print_fin
 
 print_preparacion:
 	MOVE.W			#0,D4
-	ADDQ.W			#2,D2  * Descriptor de transmision
+	ADDQ.W			#2,D2				* Descriptor de transmision
 
 print_bucle:
 	CMP.W			D4,D3
@@ -128,14 +138,14 @@ print_bucle:
 	MOVE.L			D2,D0
 	MOVE.B			(A0)+,D1
 
-	MOVE.L  		D2,-4(A6)			* Guardar descriptor modificado
-    MOVE.L  		A0,-8(A6)			* Guardar *buffer
-    MOVE.L  		D4,-12(A6)			* Guardar contador
-	BSR ESCCAR
-	MOVE.L  		-12(A6),D4			* Recuperar contador
-    MOVE.L  		-8(A6),A0			* Recuperar *buffer
-    MOVE.L  		-4(A6),D2			* Recuperar descriptor modificado
-	MOVE.L  		14(A6),D3			* Recuperar Tamano
+	MOVE.L			D2,-4(A6)			* Guardar descriptor modificado
+	MOVE.L			A0,-8(A6)			* Guardar *buffer
+	MOVE.L			D4,-12(A6)			* Guardar contador
+	BSR				ESCCAR
+	MOVE.L			-12(A6),D4			* Recuperar contador
+	MOVE.L			-8(A6),A0			* Recuperar *buffer
+	MOVE.L			-4(A6),D2			* Recuperar descriptor modificado
+	MOVE.L			14(A6),D3			* Recuperar Tamano
 
 	CMP.L			#-1,D0
 	BEQ				print_bucle_fin
@@ -145,28 +155,37 @@ print_bucle:
 
 print_bucle_fin:
 	
-	MOVE.L			D4,D0
-	CMP.W				#0,D4
+	MOVE.L			D4,D0				* Valor de retorno (caracteres escritos)
+	CMP.W			#0,D4
 	BEQ				print_fin
 
-	SUBQ.W			#2,D2
-	CMP.W			#1,D2
-	BEQ				print_reset_imr_b 
+	SUBQ.W			#2,D2				* Recuperar descriptor original
+	CMP.W			#1,D2				* Elegir línea
+	BEQ				print_reset_imr_b
 
-print_reset_imr_a: 					* Reactivar interrupciones de la DUART A
+print_reset_imr_a:						* Reactivar interrupciones de la DUART A
 	BSET			#0,IMRCP
 	MOVE.B			IMRCP,IMR
 	BRA				print_fin
 
-print_reset_imr_b:					* Reactivar interrupciones de la DUART B
+print_reset_imr_b:						* Reactivar interrupciones de la DUART B
 	BSET			#4,IMRCP
 	MOVE.B			IMRCP,IMR
 
 print_fin:
-	UNLK			A6
+	UNLK			A6					* Recuperar marco de pila
 	RTS
 
+*****************************
+************ RTI ************
+*****************************
 RTI:
 
-* Esto debe quedarse al final
+********************************************
+************ PROGRAMA PRINCIPAL ************
+********************************************
+
+**********************************
+************ INCLUDES ************
+**********************************
 INCLUDE bib_aux.s
